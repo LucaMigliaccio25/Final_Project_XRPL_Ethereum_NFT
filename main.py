828a4038edea8b237e80b63ab280b9431cfb8eba
LucaM <luca.migliaccio4@studenti.unina.it>
2025-01-05 17:21:19 +0100

    feat: Aggiunti commenti per separare variabili per XRPL da quelle per Ethereum
    - Specificato avatar_uri: URI associato ai metadati dell'immagine dell'avatar, corrispondente all'NFT creato.
    - N.B. Sarebbe stato possibile utilizzare direttamente il file contenuto nel path data/metadata/file.json.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from utils import create_and_transfer_nft
 
+# dati per XRPL
 seed_company = "sEd7uhRLEHf7sELoTUiKTcDwgn3zvdA"
 seed_receiver = "sEd7vJWGo5cYxju2raWQ1yQSFPgVejN"
 product_uri = "https://diadenn.vercel.app/product/mike-wind-1"
 taxon = 0
+
+# URI dell'avatar associato all'NFT dinamico
+avatar_uri = "https://drive.google.com/file/d/1hjWW9yfOnPk1rlcLcjg4ycADWvacXrQy/view?usp=drive_link"
 
 if __name__ == '__main__':
     wallet_receiver, NFT_token_id = create_and_transfer_nft(seed_company, product_uri, taxon, seed_receiver=seed_receiver)
